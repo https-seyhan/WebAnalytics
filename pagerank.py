7a63dc13dbfff1a9679fc1a3b420db3658b253b2
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-04-07 13:57:23 +1000

    Page Rank Example

---
+++ pagerank.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 10:37:57 2019
+
+@author: saul
+"""
+import networkx as nx
+import numpy as np
+import os
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+
+import matplotlib.pylab as plt
+
+damping = 0.85
+beta = 1 - damping
+
+os.chdir("/home/saul/pythonWork")
+
+np.seterr(divide = 'ignore')
+stmarks = nx.read_gml('stmarks.gml')
+
+print("Marks : ", stmarks)
+
+species = np.array(stmarks.nodes())
+
+print(species)
+
+Adj = nx.to_scipy_sparse_matrix(stmarks, dtype=np.float64) #graph adjacency matrix 
+
+#print(Adj)
+
+#number of species
+
+n = len(species)
+print(n)
+
+#Get degrees
+degrees = np.ravel(Adj.sum(axis=1))
+print(degrees)
+#And covert the degrees into diagonal matrix
+Deginv = sparse.diags(1 / degrees).tocsr()
+print(Deginv)
+
+Trans = (Deginv @ Adj).T
+print(Trans)
+
+I = sparse.eye(n, format ='csc')
+print(I)
+
+pagerank = spsolve(I - damping*Trans, np.full(n, beta/n))
+print(pagerank)
+
+def page_rank_plot(in_degrees, pageranks, names, *, annotations = [], **figkwargs):
+    fig, ax = plt.subplots(**figkwargs)
+    ax.scatter(in_degrees, pageranks, c=[0.835, 0.369, 0], lw=0)
+    for name, indeg, pr in zip(names, in_degrees, pageranks):
+        if name in annotations:
+            text = ax.text(indeg + 0.1, pr, name)
+    
+    ax.set_ylim(0, np.max(pageranks)*1.1)
+    ax.set_xlim(-1, np.max(in_degrees)*1.1)
+    ax.set_ylabel('PageRank')
+    ax.set_xlabel('In-degrees')
+    
+
+interesting = ['detritus', 'phytoplankton', 'benthic algea', 'micro-epiphytes',
+               'microfauna', 'zooplankton', 'predatory shrimps', 'meiofuna', 'gulls']
+
+in_degrees = np.ravel(Adj.sum(axis=0))
+page_rank_plot(in_degrees, pagerank, species, annotations = interesting)
+    
+    
+    
+    
+    
+    
