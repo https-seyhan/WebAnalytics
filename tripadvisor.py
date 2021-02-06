2568c7c153ce442a3e365255d592889941e885d5
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-07 10:26:52 +1100

    Update tripadvisor.py

--- tripadvisor.py
+++ tripadvisor.py
@@ -16,7 +16,7 @@ class TripadvisorSpider(scrapy.Spider):
 			yield scrapy.Request(absolute_url, 
 				self.parse_restaurant)
 			
-		print("Seyhan2")	
+		
 		#next_page
 		#next_page_url = response.xpath('//a[text()="Next"]').extract_first()
 		#next_absolute_url = response.urljoin(next_page_url)
