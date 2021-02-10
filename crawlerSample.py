382ad5679ce07bbb72e65e5cae99722c760f8673
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-10 21:14:08 +1100

    Update crawlerSample.py

--- crawlerSample.py
+++ crawlerSample.py
@@ -37,7 +37,7 @@ while len(crawler1.visited) < 5: # Crawler.visited is a dictionary of all URL's 
 # A higher delay (in a real-world scenario, say 30 seconds) is better:
   
 crawler2 = SimpleCrawler1(links=["http://www.clips.ua.ac.be/pages/pattern/"], domains=["ua.ac.be"], delay=0.1)
-print "CRAWLER 2 " + "-" * 50
+print ("CRAWLER 2 " + "-" * 50)
 while True:
     crawler2.crawl(cached=False)
     print ("wait...")
